bab0cfc271f82737974f0ddbca14d04e17be5954
leticiaenz <leticia.enz11@gmail.com>
2020-04-10 15:14:52 -0400

    Class 03 - Created HTTP methods

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello Letícia" });
+app.get("/projects", (request, response) => {
+  return response.json(["Projeto1", "Projeto2"]);
+});
+
+app.post("/projects", (request, response) => {
+  return response.json(["Projeto1", "Projeto2", "Projeto3"]);
+});
+
+app.put("/projects/:id", (request, response) => {
+  return response.json(["Projeto4", "Projeto2", "Projeto3"]);
+});
+
+app.delete("/projects/:id", (request, response) => {
+  return response.json(["Projeto2", "Projeto3"]);
 });
 
 app.listen(3333, () => {
